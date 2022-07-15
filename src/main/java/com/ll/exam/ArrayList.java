2bfc42e137b48284d76070eb5ee3e05015d7fdb2
Young998904 <like0998904@gmail.com>
2022-07-15 10:52:23 +0900

    get_1_equals_200

--- src/main/java/com/ll/exam/ArrayList.java
+++ src/main/java/com/ll/exam/ArrayList.java
@@ -1,16 +1,17 @@
 package com.ll.exam;
 
 public class ArrayList {
+    int[] datum = new int[3];
     int size;
     public int size() {
         return this.size;
     }
 
     public void add(int data) {
-        size++;
+        datum[size++] = data;
     }
 
-    public int get(int i) {
-        return 100;
+    public int get(int index) {
+        return datum[index];
     }
 }
