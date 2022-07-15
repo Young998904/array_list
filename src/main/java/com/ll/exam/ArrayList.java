49a28b0589d1970de250c834e4f145b383aa9b8e
Young998904 <like0998904@gmail.com>
2022-07-15 11:28:36 +0900

    최초에_배열의_크기는_2_이다

--- src/main/java/com/ll/exam/ArrayList.java
+++ src/main/java/com/ll/exam/ArrayList.java
@@ -1,13 +1,38 @@
 package com.ll.exam;
 
 public class ArrayList {
-    int[] datum = new int[3];
+    int[] datum;
     int size;
+
+    public ArrayList() {
+        size = 0;
+        datum = new int[2];
+    }
+    private void sizeUpIfFull() {
+        if ( isFull() ) {
+            sizeUp();
+        }
+    }
+
+    private void sizeUp() {
+        int[] newDatum = new int[datum.length * 2];
+
+        for ( int i = 0; i < datum.length; i++ ) {
+            newDatum[i] = datum[i];
+        }
+
+        datum = newDatum;
+    }
+
+    private boolean isFull() {
+        return size == datum.length;
+    }
     public int size() {
         return this.size;
     }
 
     public void add(int data) {
+        sizeUpIfFull();
         datum[size++] = data;
     }
 
@@ -20,5 +45,8 @@ public class ArrayList {
             datum[i] = datum[i+1];
         }
         size --;
+    }
+    public int getArrayLength() {
+        return datum.length;
     }
 }
